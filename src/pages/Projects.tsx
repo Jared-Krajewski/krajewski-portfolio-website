1342431252c8bed687bcdeae0b8d78c449b7eefb
jared krajewski <krajewski.jared@gmail.com>
2026-02-06 16:45:42 -0800

    removed duplicate ref causing deploy error

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import ProjectPost from "../components/ProjectPost";
 import { mockProjects } from "../data/portfolioData";
